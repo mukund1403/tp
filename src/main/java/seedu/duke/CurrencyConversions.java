adcb2384aeb128cd4a428d3a7498631c71172cf3
MonkeScripts <shaolianghe0.0@gmail.com>
2024-03-30 02:37:43 +0800

    Add currency exchange functionality

---
+++ src/main/java/seedu/duke/CurrencyConversions.java
@@ -0,0 +1,39 @@
+package seedu.duke;
+
+//all rates are relative to 1 SGD
+public enum CurrencyConversions {
+    USD("USD", 0.74),
+    RMB("RMB", 5.35),
+    EUR("EUR", 0.687),
+    JPY("JPY", 112.12),
+    AUD("AUD", 1.12),
+    MYR("MYR", 3.50),
+    SGD("USD", 1.00),
+
+    ;
+
+    private final String name;
+    private final double rate;
+
+    CurrencyConversions(String name, double rate) {
+        this.name = name;
+        this.rate = rate;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    double getRate() {
+        return this.rate;
+    }
+
+    double getInverseRate() {
+        return 1.00 / this.rate;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
