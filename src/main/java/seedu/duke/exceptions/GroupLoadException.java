054767d0ee2547d49ff477efdee1557502da235d
Hafizuddin Bin Aminuddin <hafizuddinaminuddin1@gmail.com>
2024-03-30 00:29:12 +0800

    Add exceptions package

---
+++ src/main/java/seedu/duke/exceptions/GroupLoadException.java
@@ -0,0 +1,7 @@
+package seedu.duke.exceptions;
+
+public class GroupLoadException extends Exception {
+    public GroupLoadException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/seedu/duke/exceptions/GroupSaveException.java
@@ -0,0 +1,7 @@
+package seedu.duke.exceptions;
+
+public class GroupSaveException extends Exception {
+    public GroupSaveException(String message) {
+        super(message);
+    }
+}
