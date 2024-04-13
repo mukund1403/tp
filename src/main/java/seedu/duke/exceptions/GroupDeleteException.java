8b58e9565988f6e5b0370fd04db2be9dea303f33
Hafizuddin Bin Aminuddin <hafizuddinaminuddin1@gmail.com>
2024-04-14 00:34:21 +0800

    Add Javadoc for delete exception

--- src/main/java/seedu/duke/exceptions/GroupDeleteException.java
+++ src/main/java/seedu/duke/exceptions/GroupDeleteException.java
@@ -1,6 +1,15 @@
 package seedu.duke.exceptions;
 
+/**
+ * Represents an exception that occurs during the deletion of a group.
+ * This exception is thrown when there is an error or failure in the group deletion process.
+ */
 public class GroupDeleteException extends Exception {
+    /**
+     * Constructs a new GroupDeleteException with the specified detail message.
+     *
+     * @param message the detail message describing the exception
+     */
     public GroupDeleteException(String message) {
         super(message);
     }
